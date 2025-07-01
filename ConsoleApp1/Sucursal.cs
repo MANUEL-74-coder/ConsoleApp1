f5a1eab71290ccd088c0eda1c3426c0f7a328c06
MANUEL-74-coder <manuelcuesaca@gmail.com>
2025-07-01 18:53:04 -0500

    cambios hechos
    jkgjkhbk

---
+++ ConsoleApp1/Sucursal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Sucursal
+    {
+        public int Id { get; set; }
+
+
+
+
+        public int EmpresaId { get; set; }
+        public int Empresa { get; set; }
+        public string Nombre { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
